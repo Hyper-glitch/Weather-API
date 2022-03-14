f1c5368b339ce1af7cb778452c7bd5995930e4ed
Roman <graff049@gmail.com>
2022-03-14 22:50:41 +0300

    1. refactor code 2. create loop for locations

--- weather_api.py
+++ weather_api.py
@@ -1,29 +1,28 @@
-from urllib.parse import urljoin
-import urllib.parse
+import urllib.parse as urllib
 
 import requests
 
 
 class WeatherApi:
-    def __init__(self, language, metric_system):
+    def __init__(self, language, measure_system):
         self.host = 'https://wttr.in'
-        self.params = {f'nTq{metric_system}': '', 'lang': f'{language}', }
+        self.params = {f'nTq{measure_system}': '', 'lang': f'{language}', }
 
-    def get_weather(self, city):
-        url = WeatherApi.create_url(city=city, host=self.host)
+    def get_weather(self, location):
+        url = WeatherApi.create_url(location=location, host=self.host)
         response = requests.get(url=url, params=self.params)
         response.raise_for_status()
-        print(response.text)
+        return response.text
 
     @staticmethod
-    def create_url(city, host):
-        encoded_city = urllib.parse.quote_plus(city)
-        url = urljoin(host, encoded_city)
-        return url
+    def create_url(location, host):
+        return urllib.urljoin(host, urllib.quote_plus(location))
 
 
 if __name__ == '__main__':
-    weather_instance = WeatherApi(language='ru', metric_system='m')
-    weather_instance.get_weather(city='london')
-    weather_instance.get_weather(city='Череповец')
-    weather_instance.get_weather(city='аэропорт Шереметьево')
+    weather_instance = WeatherApi(language='ru', measure_system='m')
+    locations = ['london', 'Череповец', 'аэропорт Шереметьево']
+
+    for location in locations:
+        weather = weather_instance.get_weather(location=location)
+        print(weather)
